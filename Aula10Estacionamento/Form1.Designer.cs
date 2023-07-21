e2808b3601a760571288c7e0ce416110bb1072bb
saramalavazi94 <139522059+saramalavazi94@users.noreply.github.com>
2023-07-21 17:13:03 -0300

    Add files via upload

---
+++ Aula10Estacionamento/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Aula10Estacionamento
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnRetirar = new System.Windows.Forms.Button();
+            this.btnEstacionar = new System.Windows.Forms.Button();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblVeiculos = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(36, 81);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(203, 171);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnFechar);
+            this.groupBox1.Controls.Add(this.btnRetirar);
+            this.groupBox1.Controls.Add(this.btnEstacionar);
+            this.groupBox1.Controls.Add(this.txtPlaca);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(38, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(269, 280);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Operações";
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(22, 200);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(214, 32);
+            this.btnFechar.TabIndex = 4;
+            this.btnFechar.Text = "Fechar estacionamento";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            // 
+            // btnRetirar
+            // 
+            this.btnRetirar.Location = new System.Drawing.Point(132, 141);
+            this.btnRetirar.Name = "btnRetirar";
+            this.btnRetirar.Size = new System.Drawing.Size(104, 32);
+            this.btnRetirar.TabIndex = 3;
+            this.btnRetirar.Text = "Retirar";
+            this.btnRetirar.UseVisualStyleBackColor = true;
+            // 
+            // btnEstacionar
+            // 
+            this.btnEstacionar.Location = new System.Drawing.Point(22, 141);
+            this.btnEstacionar.Name = "btnEstacionar";
+            this.btnEstacionar.Size = new System.Drawing.Size(104, 32);
+            this.btnEstacionar.TabIndex = 2;
+            this.btnEstacionar.Text = "Estacionar";
+            this.btnEstacionar.UseVisualStyleBackColor = true;
+            this.btnEstacionar.Click += new System.EventHandler(this.btnEstacionar_Click);
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Location = new System.Drawing.Point(20, 81);
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(216, 29);
+            this.txtPlaca.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Digite a placa:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblVeiculos);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(332, 42);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(269, 280);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Listagem de veículos";
+            // 
+            // lblVeiculos
+            // 
+            this.lblVeiculos.AutoSize = true;
+            this.lblVeiculos.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblVeiculos.Location = new System.Drawing.Point(107, 44);
+            this.lblVeiculos.Name = "lblVeiculos";
+            this.lblVeiculos.Size = new System.Drawing.Size(18, 18);
+            this.lblVeiculos.TabIndex = 2;
+            this.lblVeiculos.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Unicode MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(27, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Veículos:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 358);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private GroupBox groupBox1;
+        private Button btnFechar;
+        private Button btnRetirar;
+        private Button btnEstacionar;
+        private TextBox txtPlaca;
+        private Label label3;
+        private GroupBox groupBox2;
+        private Label lblVeiculos;
+        private Label label1;
+        private FileSystemWatcher fileSystemWatcher1;
+    }
+}
---
+++ Aula10Estacionamento/Form1.cs
@@ -0,0 +1,47 @@
+namespace Aula10Estacionamento
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> Veiculos= new List<string>();
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void adicionarPlaca()
+        {
+        
+        {
+                MessageBox.Show("O campo inserir placa está vazio");
+                return;
+        
+           
+        
+        
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void atualizarInterface()
+        {
+            int veiculos_cadastrados = veiculos.Count();
+            lblVeiculos.Text = veiculos_cadastrados.ToString();
+        }
+
+        private void btnEstacionar_Click(object sender, EventArgs e)
+        {
+            adicionarPlaca();
+
+        }
+    }
+}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }       
