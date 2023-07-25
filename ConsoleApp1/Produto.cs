ffda864b798c5678c2b73f1c0bc54f06c3115028
saramalavazi94 <139522059+saramalavazi94@users.noreply.github.com>
2023-07-25 17:14:47 -0300

    Add files via upload

---
+++ ConsoleApp1/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Produto
+    {
+        public string nome;
+        public string descricao;
+        public int quantidade;
+        public double preco;
+        public bool status;
+
+        public void mostra()
+        {
+            Console.WriteLine($"O produto se chama: {nome}, A descrição é: {descricao}, tem {quantidade}, custa R$ {preco} e status: {status}");
+        }
+
+        public void desativa()
+        {
+            quantidade = 0;
+            status = false;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+Produto lanca = new Produto();
+lanca.nome = "Diamantina";
+lanca.descricao = "Potência 10 e utilizavél";
+lanca.quantidade = 12;
+lanca.preco = 450;
+lanca.status = true;
+lanca.mostra();
+
+Produto machado = new Produto();
+machado.nome = "Fortnine";
+machado.descricao = "Força média e duração baixa";
+machado.quantidade = 45;
+machado.preco = 380;
+machado.status = true;
+machado.mostra();
+machado.desativa();
+machado.mostra();
+
+Produto espada = new Produto();
+espada.nome = "Octavia";
+espada.descricao = "Corte preciso e força unica";
+espada.quantidade = 24;
+espada.preco = 500;
+espada.status = true;
+espada.mostra();
+espada.desativa();
+espada.mostra();
