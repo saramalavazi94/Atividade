8ba0a82ac6a94760abf3dfcd80713d0fffcc6855
saramalavazi94 <139522059+saramalavazi94@users.noreply.github.com>
2023-07-25 17:14:01 -0300

    Add files via upload

---
+++ Aula12Classes/Personagem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula12Classes
+{
+    internal class Personagem
+    {
+        // VARIÁVEIS (atributos)
+       public string nome;
+       public string raca;
+       public int poder;
+
+        // FUNÇÕES (métodos)
+       public void mostra()
+       {
+            Console.WriteLine($"Seu personagem é {raca} o nome é {nome} e o poder é {poder} ");
+       }
+    }
+}
---
+++ Aula12Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Aula12Classes;
+
+Console.WriteLine("Hello, World!");
+
+
+Personagem guerreiro = new Personagem();
+guerreiro.nome = "Jacob";
+guerreiro.raca = "Anjo";
+guerreiro.poder = 72;
+
+guerreiro.mostra();
+
+Personagem mago = new Personagem();
+mago.raca = "bruxo";
+mago.poder = 180;
+mago.nome = "kell";
+
+mago.mostra();
+
+Personagem bruxo = mago;
+bruxo.nome = "Eco";
+bruxo.mostra();
+
+Personagem cacador = new Personagem();
+cacador.nome = "Jeremy";
+cacador.raca = "Humano";
+cacador.poder = 412;
+
+cacador.mostra();
+Console.ReadLine();
